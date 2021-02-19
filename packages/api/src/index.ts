4d196ad1b399eac65363caeb32c1bd3c1ee736b9
Ryan <ryan@sylver.me>
2021-02-19 11:42:28 +0800

    return json errors on api routes

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,4 +1,4 @@
-import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
+import { ClassSerializerInterceptor, ValidationPipe, InternalServerErrorException } from "@nestjs/common";
 import { NestFactory, Reflector } from "@nestjs/core";
 import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
 import fastifyMultipart from "fastify-multipart";
@@ -6,6 +6,8 @@ import { createStartupInvite } from "./helpers/createStartupInvite";
 import { AppModule } from "./modules/AppModule";
 import { config } from "./config";
 import cookie from "fastify-cookie";
+import { RenderService } from "nest-next";
+import { FastifyRequest, FastifyReply } from "fastify";
 
 async function main() {
   // some routes use jwts in params and they can get very long, hence maxParamLength
@@ -15,6 +17,7 @@ async function main() {
   // should probably move this to per-controller or per-route for perf but it's fine for now
   app.useGlobalInterceptors(new ClassSerializerInterceptor(new Reflector()));
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   app.register(cookie);
   app.register(fastifyMultipart, {
     limits: {
@@ -27,7 +30,13 @@ async function main() {
     },
   });
 
-  app.enableCors();
+  const service = app.get(RenderService);
+  service.setErrorHandler(async (err, request: FastifyRequest, reply: FastifyReply) => {
+    if (request.url.startsWith("/api")) {
+      const error = err.response ? err : new InternalServerErrorException(err.message);
+      return reply.status(error.status).send(error.response);
+    }
+  });
 
   await app.listen(8080, "0.0.0.0");
   await createStartupInvite();
