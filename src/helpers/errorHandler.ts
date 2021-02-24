c3f88d6c2f91d061654ba1e6d85db788ece468ef
Ryan <ryan@sylver.me>
2021-02-25 02:51:09 +0800

    fix error handler again >:(

--- src/helpers/errorHandler.ts
+++ src/helpers/errorHandler.ts
@@ -1,4 +1,4 @@
-import { HttpException, InternalServerErrorException, Logger } from "@nestjs/common";
+import { HttpException, HttpStatus, InternalServerErrorException, Logger } from "@nestjs/common";
 import { FastifyRequest } from "fastify";
 import { RenderableReply } from "../types";
 
@@ -7,17 +7,18 @@ export const errorHandler = async (err: any, request: FastifyRequest, reply: Ren
   const wrapped = err instanceof HttpException ? err : new InternalServerErrorException(err.message);
   const response = wrapped.getResponse() as any;
   const status = wrapped.getStatus();
-  if (status >= 500) logger.error(err.message, err.stack);
+  if (status >= HttpStatus.INTERNAL_SERVER_ERROR) {
+    logger.error(err.message, err.stack);
+  }
+
   if (request.url.startsWith("/api")) {
     return reply.status(status).send(response);
   }
 
-  if (status === 404) {
-    return reply.render("404");
+  if (status !== HttpStatus.NOT_FOUND) {
+    return reply.render("_error", {
+      title: response.error === response.message ? response.statusCode.toString() : response.error,
+      message: response.message,
+    });
   }
-
-  return reply.render("_error", {
-    title: response.error === response.message ? response.statusCode.toString() : response.error,
-    message: response.message,
-  });
 };
