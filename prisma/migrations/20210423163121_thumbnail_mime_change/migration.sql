533b542372a51e3c9dae0146795dad7b0dcf18a5
Ryan <ryan@sylver.me>
2021-04-29 04:02:30 +0800

    fix bad migration

--- prisma/migrations/20210423163121_thumbnail_mime_change/migration.sql
+++ prisma/migrations/20210423163121_thumbnail_mime_change/migration.sql
@@ -1,4 +1,4 @@
 -- we're changing the type of thumbnails from image/jpeg to image/webp for alpha support
 -- and since we dont store thumbnail mime types the choices were store them or wipe old jpeg thumbnails
 -- and this seemed the better option.
-DELETE * FROM "thumbnails";
+TRUNCATE "thumbnails";
