c31510678b419c65da600a40a1dfc54d994c6d3f
sylv <ryan@sylver.me>
2022-06-19 03:16:17 +0800

    fix(web): hosts list

--- packages/web/src/components/host-list.tsx
+++ packages/web/src/components/host-list.tsx
@@ -1,8 +1,7 @@
 import { Listbox } from "@headlessui/react";
 import classNames from "classnames";
 import { FC, Fragment, useRef, useState } from "react";
-import { ChevronDown, ChevronUp } from "react-feather";
-import { useOnClickOutside } from "../hooks/use-on-click-outside.helper";
+import { Check, ChevronDown } from "react-feather";
 import { inputClasses, InputContainer } from "./input/input-container";
 
 export interface HostListProps {
@@ -17,60 +16,42 @@ export interface HostListProps {
 // todo: this is kind of hacked together until
 // https://github.com/tailwindlabs/headlessui/pull/648 is merged
 export const HostList: FC<HostListProps> = (props) => {
-  const [open, setOpen] = useState(false);
   const [selectedHosts, setSelectedHosts] = useState<string[]>([props.hosts[0]]);
   const reference = useRef<HTMLDivElement>(null);
-  const Icon = open ? ChevronUp : ChevronDown;
 
-  useOnClickOutside(reference, () => setOpen(false));
-
-  const isSelected = (value: string) => selectedHosts.includes(value);
-  const onChange = (value: string) => {
-    if (!isSelected(value)) {
-      const withValue = [...selectedHosts, value];
-      setSelectedHosts(withValue);
-      props.onChange(withValue);
-    } else {
-      const withoutValue = selectedHosts.filter((host) => host !== value);
-      setSelectedHosts(withoutValue);
-      props.onChange(withoutValue);
-    }
+  const onChange = (selected: string[]) => {
+    setSelectedHosts(selected);
   };
 
   return (
     <div ref={reference} className="relative z-10">
-      <Listbox as={Fragment} value={selectedHosts as unknown as string} onChange={onChange}>
-        <InputContainer
-          className="cursor-pointer"
-          onClick={() => setOpen(!open)}
-          prefix={props.prefix}
-          suffix={props.suffix}
-        >
-          <div className={classNames(inputClasses, "flex items-center justify-between select-none overflow-hidden")}>
-            <span className="truncate">{selectedHosts.join(", ")}</span>
-            <Icon className="text-gray-300" />
-          </div>
-        </InputContainer>
-        {open && (
-          <Listbox.Options
-            className="absolute right-0 mt-2 overflow-y-auto rounded-md shadow-2xl bg-dark-300 focus:outline-none max-h-56 min-w-[10em] w-full overflow-y-auto"
-            static
-          >
-            {props.hosts.map((host) => (
-              <Listbox.Option as={Fragment} key={host} value={host}>
-                {({ active }) => {
-                  const highlight = active || isSelected(host);
-                  const classes = classNames(
-                    "text-white bg-dark-600",
-                    highlight && "text-white bg-dark-600",
-                    "select-none"
-                  );
-                  return <div className={classes}>{host.replace("{{username}}", props.username)}</div>;
-                }}
-              </Listbox.Option>
-            ))}
-          </Listbox.Options>
-        )}
+      <Listbox as={Fragment} value={selectedHosts} multiple onChange={onChange}>
+        <Listbox.Button as={Fragment}>
+          <InputContainer className="cursor-pointer" prefix={props.prefix} suffix={props.suffix}>
+            <div className={classNames(inputClasses, "flex items-center justify-between select-none overflow-hidden")}>
+              <span className="truncate">{selectedHosts.join(", ")}</span>
+              <ChevronDown className="text-gray-300" />
+            </div>
+          </InputContainer>
+        </Listbox.Button>
+        <Listbox.Options className="absolute right-0 mt-2 overflow-y-auto rounded-md shadow-2xl bg-dark-300 focus:outline-none max-h-56 min-w-[10em] w-full overflow-y-auto">
+          {props.hosts.map((host) => (
+            <Listbox.Option
+              key={host}
+              value={host}
+              className={({ active, selected }) =>
+                classNames(`select-none px-2 py-1 text-gray-500`, selected && "!text-gray-300", active && "bg-dark-600")
+              }
+            >
+              {({ selected }) => (
+                <span className="flex items-center gap-2">
+                  {host.replace("{{username}}", props.username)}{" "}
+                  {selected && <Check className="text-purple-400 h-4 w-4" />}
+                </span>
+              )}
+            </Listbox.Option>
+          ))}
+        </Listbox.Options>
       </Listbox>
     </div>
   );
