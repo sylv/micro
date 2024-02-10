8236d9ef42f62fa3c3b0252dfdac75ca333fb90a
Sylver <ryan@sylver.me>
2024-02-11 00:35:20 +0800

    fix: bind to 0.0.0.0 in docker

--- wrapper.sh
+++ wrapper.sh
@@ -9,7 +9,7 @@ cleanup() {
 trap cleanup EXIT HUP INT QUIT PIPE TERM ERR
 
 cd packages/api && npm run start &
-cd packages/web && npm run start &
+cd packages/web && HOST=0.0.0.0 npm run start &
 
 wait -n
 exit $?
