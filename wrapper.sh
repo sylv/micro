ef18709612ab823f59758949ea7bb877263696ac
Sylver <ryan@sylver.me>
2024-02-11 12:59:27 +0800

    fix: forgot that was necessary

--- wrapper.sh
+++ wrapper.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+cleanup() {
+    pkill -P $$
+}
+
+trap cleanup EXIT HUP INT QUIT PIPE TERM ERR
+
 (cd packages/api && npm run start) &
 (cd packages/web && HOST=0.0.0.0 npm run start) &
 
